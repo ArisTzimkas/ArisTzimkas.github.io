adca72fddef80a2ec903efd9ef4b2cb6485d02e4
Aris Tzimkas <aristeidhtz@gmail.com>
2025-10-04 21:10:24 +0300

    removed fade in effect from projects

--- app.js
+++ app.js
@@ -26,20 +26,6 @@ VANTA.FOG({
     })
 
 
-const observer=new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if(entry.isIntersecting){
-            console.log(entry.target)
-            entry.target.classList.add('show');
-        }
-        else{
-            entry.target.classList.remove('show');
-        }
-    })
-}, {});
-
-const todoElements=document.querySelectorAll('.project-item');
-todoElements.forEach(el=>observer.observe(el));
 
 
 
